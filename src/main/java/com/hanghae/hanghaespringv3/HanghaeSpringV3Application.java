f537d9f322281993fd73a09011689b4b581ddd98
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-02-04 18:41:24 +0900

    Initial commit

---
+++ src/main/java/com/hanghae/hanghaespringv3/HanghaeSpringV3Application.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaespringv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeSpringV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeSpringV3Application.class, args);
+    }
+
+}
